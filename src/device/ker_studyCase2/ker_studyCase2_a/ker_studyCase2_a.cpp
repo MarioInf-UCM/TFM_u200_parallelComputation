07190d30ad3f6cae0077bb0d33c511e1f8e96016
MarioInf-UCM <marioa25@ucm.es>
2024-08-02 16:07:26 +0200

    Implemented python files to study case 1 and 2

--- src/device/ker_studyCase2/ker_studyCase2_a/ker_studyCase2_a.cpp
+++ src/device/ker_studyCase2/ker_studyCase2_a/ker_studyCase2_a.cpp
@@ -53,8 +53,8 @@ extern "C"{
     #endif
     {
         #pragma HLS INTERFACE m_axi port = inD_vA offset = slave bundle = gmem
-        #pragma HLS INTERFACE m_axi port = inD_vB offset = slave bundle = gmem
-        #pragma HLS INTERFACE m_axi port = outD_result offset = slave bundle = gmem 
+        #pragma HLS INTERFACE m_axi port = inD_vB offset = slave bundle = gmem1
+        #pragma HLS INTERFACE m_axi port = outD_result offset = slave bundle = gmem2
 
         #pragma HLS INTERFACE s_axilite port = inD_vA bundle = control
         #pragma HLS INTERFACE s_axilite port = inD_vB bundle = control
